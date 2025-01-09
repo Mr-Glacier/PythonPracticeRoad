c1ef52584bb4327e2ba3d5368810ce9a1c7c88c7
Mr-Glacier <K15100415112@163.com>
2025-01-09 17:04:15 +0800

    add 邮箱解密方案

--- JournalCrawler/wonderfulTools.py
+++ JournalCrawler/wonderfulTools.py
@@ -1,4 +1,6 @@
 import os
+import re
+from urllib.parse import unquote
 
 
 # 个人奇妙小工具
@@ -24,3 +26,19 @@ def read_text_from_file(file_path):
         return text
     except Exception as e:
         print(f"An error occurred while trying to read the file: {e}")
+
+
+# 解密邮箱加密
+def Decryption_email(cfemail: str):
+    email_list = re.findall(r'.{2}', cfemail)
+    key = email_list[0]
+    ll = []
+    for e in email_list[1:]:
+        # 对十六进制进行异或运算
+        r = hex(int(key, 16) ^ int(e, 16))
+        ll.append(r)
+    # 拼接运算后的字符串
+    a = ''.join(ll)
+    # URL解码字符串
+    email = unquote(a.replace('0x', '%'))
+    return email
